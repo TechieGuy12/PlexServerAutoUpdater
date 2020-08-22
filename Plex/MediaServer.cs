e66329ba1a35fac3ba79bc964d08bc70c779aa93
TechieGuy12 <psalmon12@gmail.com>
2020-08-22 00:05:03 -0400

    Moved UpdateChannel enum

--- Plex/MediaServer.cs
+++ Plex/MediaServer.cs
@@ -13,6 +13,21 @@ using System.Threading.Tasks;
 namespace TE.Plex
 {
     /// <summary>
+    /// The type of installation package.
+    /// </summary>
+    public enum UpdateChannel
+    {
+        /// <summary>
+        /// Public released package.
+        /// </summary>
+        Public = 0,
+        /// <summary>
+        /// Plex Pass-only package.
+        /// </summary>
+        PlexPass = 8
+    }
+
+    /// <summary>
     /// Description of MediaServer.
     /// </summary>
     public class MediaServer
@@ -352,7 +367,6 @@ namespace TE.Plex
             serviceUserSid = plexService.LogOnUser.Sid;
 
             string token = plexRegistry.GetToken();
-
             if (token == null)
             {
                 OnUpdateMessage("Could not get the latest install package.");
